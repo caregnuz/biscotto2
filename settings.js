1d958267f3486fbea6fe4f20fbece70a89ce45b6
caregnuz <caregnatocristian865@gmail.com>
2025-03-18 21:21:51 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "biscotto Bot",
   botOwner: 'cristian', // Your name
-  ownerNumber: '+639654978027', //Your number
+  ownerNumber: '+399510030546', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
